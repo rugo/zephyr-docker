f3965a53d820d7a2e0d9d46934cc1d3c28ed9de0
Ruben Gonzalez <rg@ht11.org>
2022-01-27 13:55:44 +0100

    Add --rebuild switch to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,16 +7,28 @@ WORKSPACE_NAME=kemtls-experiment
 WORKSPACE_BASE_DIR=/root/workspaces
 WORKSPACE_DIR=${WORKSPACE_BASE_DIR}/${WORKSPACE_NAME}
 WORKSPACE_URL="https://git.fslab.de/rgonza2s/zephyr-project.git"
+BUILD_DIR=${WORKSPACE_DIR}/build
 
 if [ ! -d $WORKSPACE_DIR ]; then
     echo "No '$WORKSPACE_DIR' folder. Initializing workspace from ${WORKSPACE_URL}."
     mkdir -p ${WORKSPACE_DIR}
     cd ${WORKSPACE_DIR}
     west init -m ${WORKSPACE_URL}
+    west update
     cd
 fi
 
+if [ "$1" == "--rebuild" ]; then
+    cd ${WORKSPACE_DIR}
+    if [ -d ${BUILD_DIR} ]; then
+        echo "Erasing build dir"
+        rm -rf $BUILD_DIR
+    fi
+    echo "Updating dependencies"
+    west update
+    cd
+fi
+
+
 cd $WORKSPACE_DIR
-rm -rf build
-west update
 west build -p auto -b efm32gg_stk3701a modules/crypto/wolfssl/zephyr/samples/wolfssl_tls_sock/
